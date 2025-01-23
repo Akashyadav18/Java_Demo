0814a239c6e04c3c3a7d428aec0115a9564f6502
Akashyadav18 <shadowlee5469@gmail.com>
2025-01-23 20:08:32 +0530

    SpringBoot Demo

---
+++ SpringBoot_Demo_6_1/src/main/java/in/sp/main/SpringBootDemo61Application.java
@@ -0,0 +1,13 @@
+package in.sp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo61Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo61Application.class, args);
+	}
+
+}
